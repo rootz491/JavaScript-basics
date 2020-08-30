116a83a6e0b90d88c7a12d24677dc10ccfd78397
rootz491 <experimentyt1@gmail.com>
2020-08-30 14:01:41 +0530

    compeleted ToDo project

--- todo.js
+++ todo.js
@@ -1,22 +1,41 @@
-const todo = [
-	"abc", 
-	"xyz"
-];
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+let deletebtn = document.getElementById('deleteBtn');
 
-const createTask = () => {
-    let task = document.getElementById("task").value;
-	addTask(task);
-}
+function additem(e) {
+	e.preventDefault();
+	
+	//add item value
+	let newitem = document.getElementById('addTask').value;
+	if(newitem) {
+		//create new li element
+		let li = document.createElement('li');
+		//add id
+		li.id = 'items';
+		//add text node
+		li.appendChild(document.createTextNode(newitem));
+		//create new deleteBtn
+		let newDeleteBtn = document.createElement('button');
+		//add class
+		newDeleteBtn.className = 'deleteBtn';
+		//add ID
+		newDeleteBtn.id = 'deleteBtn'
+		//apppend deleteBtn to li
+		li.appendChild(newDeleteBtn);
+		//append li
+		itemList.appendChild(li);
+	}
+}	
 
-const addTask = (task) => {
-    todo.push(task);
-}
-
-const showTask = () => {
-	for (var i = 0; i < todo.length; i++) {
-	//	document.getElementById("taskDisplay")[i].style.display = block;
-	//	document.getElementById("taskDisplay")[i].innerHTML = task[i];			
-
-		document.appendChild('li')[i];	
+function removeItem(e) {
+	if(e.target.classList.contains('deleteBtn')) {
+		if(confirm('are you sure?')) {
+			var li = e.target.parentElement;
+			itemList.removeChild(li);
+		}
 	}
 }
+
+//	form submit event
+form.addEventListener('submit', additem);
+itemList.addEventListener('click', removeItem); 
