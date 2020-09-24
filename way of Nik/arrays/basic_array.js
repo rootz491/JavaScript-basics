c08dbfd98f0bdc5fd22d495610946d4312130765
rootz491 <experimentyt1@gmail.com>
2020-09-24 20:48:23 +0530

    more on arrays

--- way of Nik/arrays/basic_array.js
+++ way of Nik/arrays/basic_array.js
@@ -1,6 +1,6 @@
 const array = ['mango', 'apple', 'banana', 'orange'];
 console.log(array);
-
+/*
 array.push('kiwi');         //  add element to the end of the array.
 array.pop();                //  remove element from the end of the array.
 array.shift();              //  remove element from start of array.
@@ -46,7 +46,55 @@ console.log(array3);
 array3.length = 5;                          //  reassigning the array.lenght will actually affect the real array.
 console.log(array3);
 
+*/
 
+/*
+
+//      tale of array splice property
+
+array.splice(2, 0, 'new here');                 //  it will add new element to 2nd index
+console.log(array);
+
+array.splice(2, 1);                             //  remove that element from second place
+console.log(array);
+
+array.splice(array.length-1, 1, 'replaced');    //  replace the last element of array
+console.log(array);
+
+array.splice(10, 0, 'weirdo');
+console.log(array);
+
+*/
+
+/*          POWER OF ARRAY LENGTH PROPERTY
+
+
+console.log(array.length)           //  some length of array, say 5         
+array.length=4                      //  modify lenght or length -= 1
+console.log(array);                 //  last element is no more.                        [ 'mango', 'apple', 'banana', 'replaced' ]
+array.length++                      //  then, increase the length or length += 1
+console.log(array);                 //  see, the removed array wont come back!          [ 'mango', 'apple', 'banana', 'replaced', <1 empty item> ]
+
+
+*/
+
+//  static functions
+
+var array1 = Array.from(array);     //  create new array
+//  console.log(array1);
+
+var array2 = Array.isArray();       //  check if it's an array or not!
+//  console.log(array2);
+
+var array3 = Array.of(array1, array, 1, 2, 3, 4, {"karan sharma" : true})
+//  console.log(array3);
+
+
+
+//  instance functions
+
+var array4 = Array.prototype.concat(array1, array2, array3);
+console.log(array4);
 
 
 
