6650a0ab16469a089b97a777124c04f16f0c1da2
rootz491 <experimentyt1@gmail.com>
2020-10-25 18:02:36 +0530

    classes revised

--- js-concepts/classes.js
+++ js-concepts/classes.js
@@ -1,56 +1,69 @@
-
 class person {
     constructor(name, aged) {
         this.name = name;
         this.age = aged;
         this.hi = () => {
             console.log(`Hii, ${this.name}`);
-            const how = () => console.log(`you're ${this.age} years old!`);
-            how();
+            //  how();
         }
-        this.hi();
+        //  this.hi();
     }
-    myName = () => {
-        console.log(`My name is ${this.name}`);
-        const myAge = () => console.log(`I'm ${this.age} year old!`);
-        myAge();
-    };
+
+    myAge() {
+        console.log(`I'm ${this.age} year old!`);
+    }
 }
 
-me = new person("karan", 18);
 
 class student extends person {
     constructor(name, age, hobby, fav) {
         super(name,age);
-        const data = {
-            name,
-            age,
-            hobby,
-            favourate_lang : fav
-        }
-        console.log(data);    
+        this.hobby = hobby;
+        this.fav = fav;
     }
-}
 
+    myDegree() {
+        console.log('ima Computer Science student.');
+    }
+
+    myHubby() {
+        console.log(`my hobby is ${this.hobby}`);
+    }
+
+    myFav() {
+        console.log(`my fav lang. is ${this.fav}`);
+    }
+} 
+
+//  use of base class
+me = new person("karan", 18);
+me.hi();
+me.myAge();
+
+
+//  use of inherited class
 nikhil =  new student("Nik", 19, "coding", "Javascript");
-
+nikhil.hi();
+nikhil.myAge();
+nikhil.myDegree();
+nikhil.myFav();
+nikhil.myHubby();
 
 
 //----------------------------------------------------
-
+/*
 
 if(NaN !== NaN) {
     console.log("fuck");
 }
 
-
-
+*/
 //-----------------------------------------------------
 
 //  chaining function
 
 
 const phrase = "I am a sentence";
-console.log(phrase)
+// console.log(phrase)
 const flip = phrase.split(" ").reverse().join(" ");
-console.log(flip);
+// console.log(flip);
