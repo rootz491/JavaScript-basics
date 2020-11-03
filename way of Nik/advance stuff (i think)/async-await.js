2911e8a2ddaa9a7ace5ae8b10f33a725baced6d2
rootz491 <experimentyt1@gmail.com>
2020-11-03 17:38:31 +0530

    still await

--- way of Nik/advance stuff (i think)/async-await.js
+++ way of Nik/advance stuff (i think)/async-await.js
@@ -1,18 +1,56 @@
-let hello = async function() {
-    return 'hello';
+//  writing async in front of function makes it return a promise.
+//  if we return a constant (it will be returned as resolved promise);
+
+let bob = async function () {
+    return 20;
 }
 
-// console.log(hello());
+bob().then(data => console.log(data));
 
-hello().then(value => {
-    // console.log(value);
-})
+//  here we have returned a whole promise.
 
-//  ---------------------------------------------------
-
-let second = async () => {
-    let data = setTimeout(() => {
-        console.log('hahaha');
-    }, 3000);
-    return data
+let oggy = async function (car) {
+    return new Promise(function (resolve, reject) {
+        if(car == 'tesla') {
+            console.log('elon musk');
+        }
+        else {
+            console.log('fuck you');
+        }
+    })
 }
+
+oggy('tesla')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+
+//  using await, we can delay the execution of async code untill promise is not resolved.
+///*
+
+let savvy = async function () {
+    let result = new Promise((resolve, reject) => {
+        if(1 == 1.0)    resolve(true);
+        else    reject(false);
+    });
+    let done  = await result;
+    console.log(done);
+}
+
+savvy()
+
+//*/
+    
+async function firstAsync() {
+    let promise = new Promise((res, rej) => {
+        setTimeout(() => res("Now it's done!"), 1000)
+    });
+    
+    // wait until the promise returns us a value
+    let result = await promise; 
+    
+    // "Now it's done!"
+    console.log(result); 
+}
+    
+firstAsync();
